986f58e726fc3c64aebaa3fba06e10f988d3a2c7
Andrew MacLeay <amacleay@athenahealth.com>
2016-11-27 20:18:15 -0500

    More about us, rough schedule

--- src/containers/AboutUsSection.jsx
+++ src/containers/AboutUsSection.jsx
@@ -6,8 +6,19 @@ export default React.createClass({
     return (
       <div className="section">
         <h1>About Us</h1>
-        <p className="text-info">Coming soon!</p>
-        <div className="abby-in-cow-suit" />
+        <p>
+          We began our journey together in the summer of 2011, both working at <a href="https://yea.org/programs/cadets/cadets">the Cadets</a> drum corps.  We spent a couple of difficult years apart, flying frequently to see one another, but in the summer of 2013, Abigail made the move to Medford, Massachusetts.  We've been causing trouble together in eastern Mass ever since.
+        </p>
+        <div><img src='/images/abi-cow.jpg' className="abby-cow" /></div>
+        <p>
+          In the summer of 2015, both of us were on the road on the teaching staff of the Cadets, among many of the same people we'd been with when we met back in 2011.  On finals day, during the final daily staff meeting of the season, Andrew screwed up his courage and asked a question that had been on his mind for the past four years:
+        </p>
+        <div>
+          <video controls>
+            <source src="/images/engage.mp4" type="video/mp4"></source>
+            Your browser does not support native video.
+          </video>
+        </div>
       </div>
     );
   },
--- src/containers/ScheduleSection.jsx
+++ src/containers/ScheduleSection.jsx
@@ -4,8 +4,83 @@ export default React.createClass({
   render() {
     return (
       <div className="section">
-        <h1>Schedule</h1>
-        <p className="text-info">Coming soon!</p>
+        <h3>A Weekend Adventure</h3>
+        <p>
+          Our wedding weekends begins on Friday, September 22 and will continue through the early afternoon on Sunday, September 24.  We sincerely hope you will be able to spend the entire time with us celebrating this important occasion!  While there are accomodations nearby, we promise that staying with us in the camp will be more relaxing and more fun than any other the alternatives!
+        </p>
+        <p>
+          Please aim to arrive Friday evening so you can be present for the first night's dinner and campfire fun.  There will be ample parking and we will make arrangements to ensure an easy check-in to the camp.  Once on the camp property, please plan on being able to get around the small property without a motor vehicle. <i>Please let us know if this presents a problem - we will make arrangements to ensure everyone's comfort</i>.
+        </p>
+
+        <h3>Schedule</h3>
+        <p className="text-info">
+          <i>Please note that food and drink will be provided throughout the weekend by the capable camp staff.</i>
+        </p>
+
+        <div className="schedule-day-container">
+          <h4>Friday</h4>
+
+          <ul className="schedule-event-list">
+            <li className="schedule-event-item">
+              <div className="schedule-event-time">4-6 pm</div>
+              <div className="schedule-event-name">Guests arrive</div>
+            </li>
+            <li className="schedule-event-item">
+              <div className="schedule-event-time">6 pm</div>
+              <div className="schedule-event-name">Barbecue dinner</div>
+            </li>
+            <li className="schedule-event-item">
+              <div className="schedule-event-time">8 pm</div>
+              <div className="schedule-event-name">Campfire and s'mores</div>
+            </li>
+          </ul>
+        </div>
+
+        <div className="schedule-day-container">
+          <h4>Saturday</h4>
+
+          <ul className="schedule-event-list">
+            <li className="schedule-event-item">
+              <div className="schedule-event-time">8 am</div>
+              <div className="schedule-event-name">Breakfast</div>
+            </li>
+            <li className="schedule-event-item">
+              <div className="schedule-event-time">9 am</div>
+              <div className="schedule-event-name">Activity time</div>
+            </li>
+            <li className="schedule-event-item">
+              <div className="schedule-event-time">12 pm</div>
+              <div className="schedule-event-name">Lunch</div>
+            </li>
+            <li className="schedule-event-item">
+              <div className="schedule-event-time">1 pm</div>
+              <div className="schedule-event-name">Activity time</div>
+            </li>
+            <li className="schedule-event-item">
+              <div className="schedule-event-time">5:30 pm</div>
+              <div className="schedule-event-name">Wedding ceremony</div>
+            </li>
+            <li className="schedule-event-item">
+              <div className="schedule-event-time">6 pm</div>
+              <div className="schedule-event-name">Cocktail hour</div>
+            </li>
+            <li className="schedule-event-item">
+              <div className="schedule-event-time">7 pm</div>
+              <div className="schedule-event-name">Reception - dinner, music, and dancing</div>
+            </li>
+          </ul>
+        </div>
+
+        <div className="schedule-day-container">
+          <h4>Sunday</h4>
+
+          <ul className="schedule-event-list">
+            <li className="schedule-event-item">
+              <div className="schedule-event-time">8-11 am</div>
+              <div className="schedule-event-name">Brunch</div>
+            </li>
+          </ul>
+        </div>
       </div>
     );
   },
